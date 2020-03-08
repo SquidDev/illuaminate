f7e45dde82e2482faed7e6e595fbd42602dec6c3
SquidDev <git@squiddev.cc>
2020-03-08 16:59:11 +0000

    Initial vscode support

---
+++ src/vscode/extension.ts
@@ -0,0 +1,36 @@
+import { ExtensionContext, commands, workspace } from "vscode";
+import { LanguageClient, LanguageClientOptions, ServerOptions } from "vscode-languageclient";
+
+let client: LanguageClient | undefined;
+
+const startServer = () => {
+  // Spin up a new server
+  const command = workspace.getConfiguration("illuaminate").get<string>("executable", "illuaminate-lsp");
+  const serverOptions: ServerOptions = { command };
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ language: "lua" }],
+    synchronize: {
+      // Also notify the server about changes to .lua files.
+      fileEvents: workspace.createFileSystemWatcher("**/.lua"),
+    },
+  };
+
+  client = new LanguageClient("illuaminate", "illuaminate Language Server", serverOptions, clientOptions);
+  client.start();
+};
+
+const stopServer = () => client ? client.stop() : Promise.resolve();
+
+export const activate = (context: ExtensionContext) => {
+  startServer();
+
+  context.subscriptions.push(commands.registerCommand("illuaminate.restartServer", async () => {
+    await stopServer();
+    startServer();
+  }));
+};
+
+export const deactivate = (): Thenable<void> | undefined => {
+  if (!client) return undefined;
+  return client.stop();
+};
