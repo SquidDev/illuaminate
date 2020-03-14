81b6b841d1370b9f2b0dfa693459f2f3022d6e68
Jonathan Coates <git@squiddev.cc>
2020-03-14 13:07:19 +0000

    Port the data tests over to Alcotest too

--- test/data.ml
+++ test/data.ml
@@ -1,4 +1,4 @@
-open Omnomnom.Tests
+open Alcotest
 open IlluaminateData
 
 let ref_key = Key.deferred ~name:"Reference" ()
@@ -11,69 +11,57 @@ let count_key key k =
 
 let ref_builder = Builder.oracle ref_key (fun x _ -> !x)
 
-let ( let+ ) x f =
-  match x with
-  | { outcome = Pass; _ } -> f ()
-  | x -> x
-
-let assert_eq ~pp exp actual =
-  if exp == actual then result Pass
-  else
-    result
-      ~message:(fun f -> Format.fprintf f "Expected %a, got %a" pp exp pp actual)
-      (Failed { backtrace = Some (Printexc.get_raw_backtrace ()) })
-
-let pp_int f = Format.fprintf f "%d"
+let test_case t s f = OmnomnomAlcotest.of_alcotest_case (test_case t s f)
 
 let tests =
-  group "The data/incremental system"
-    [ ( test "Do not rebuild on constant builds" @@ fun () ->
+  Omnomnom.Tests.group "The data/incremental system"
+    [ ( test_case "Do not rebuild on constant builds" `Quick @@ fun () ->
         let a = ref 1 and b = ref 2 in
         let changed_key, changed = count_key add_key (a, b) in
         let data = Builder.(empty |> ref_builder |> build) in
 
         (* An initial build should result in 3 and always change. *)
-        let+ () = assert_eq ~pp:pp_int 3 (get data changed_key ()) in
-        let+ () = assert_eq ~pp:pp_int 1 !changed in
+        check int "Computed value" 3 (get data changed_key ());
+        check int "Changed" 1 !changed;
 
         (* Swap the two: we shouldn't rebuild the final result. *)
         refresh data;
-        let+ () = assert_eq ~pp:pp_int 3 (get data changed_key ()) in
-        let+ () = assert_eq ~pp:pp_int 1 !changed in
+        check int "Computed value" 3 (get data changed_key ());
+        check int "Did not change" 1 !changed;
 
-        result Pass );
-      ( test "Do not rebuild on non-changing builds" @@ fun () ->
+        () );
+      ( test_case "Do not rebuild on non-changing builds" `Quick @@ fun () ->
         let a = ref 1 and b = ref 2 in
         let changed_key, changed = count_key add_key (a, b) in
         let data = Builder.(empty |> ref_builder |> build) in
 
         (* An initial build should result in 3 and always change. *)
-        let+ () = assert_eq ~pp:pp_int 3 (get data changed_key ()) in
-        let+ () = assert_eq ~pp:pp_int 1 !changed in
+        check int "Computed value" 3 (get data changed_key ());
+        check int "Changed" 1 !changed;
 
         (* Swap the two: we shouldn't rebuild the final result. *)
         a := 2;
         b := 1;
         refresh data;
-        let+ () = assert_eq ~pp:pp_int 3 (get data changed_key ()) in
-        let+ () = assert_eq ~pp:pp_int 1 !changed in
+        check int "Computed value" 3 (get data changed_key ());
+        check int "Did not change" 1 !changed;
 
-        result Pass );
-      ( test "Rebuild on non-changing builds" @@ fun () ->
+        () );
+      ( test_case "Rebuild on non-changing builds" `Quick @@ fun () ->
         let a = ref 1 and b = ref 2 in
         let changed_key, changed = count_key add_key (a, b) in
         let data = Builder.(empty |> ref_builder |> build) in
 
         (* An initial build should result in 3 and always change. *)
-        let+ () = assert_eq ~pp:pp_int 3 (get data changed_key ()) in
-        let+ () = assert_eq ~pp:pp_int 1 !changed in
+        check int "Computed value" 3 (get data changed_key ());
+        check int "Changed" 1 !changed;
 
         (* Swap the two: we shouldn't rebuild the final result. *)
         a := 2;
         b := 3;
         refresh data;
-        let+ () = assert_eq ~pp:pp_int 5 (get data changed_key ()) in
-        let+ () = assert_eq ~pp:pp_int 2 !changed in
+        check int "Computed value" 5 (get data changed_key ());
+        check int "Changed" 2 !changed;
 
-        result Pass )
+        () )
     ]
