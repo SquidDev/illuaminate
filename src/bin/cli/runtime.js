cf008dbd7607a7bc92c80f8281f59196f1198388
Jonathan Coates <git@squiddev.cc>
2020-04-30 19:38:31 +0100

    Update for latest js_of_ocaml

--- src/bin/cli/runtime.js
+++ src/bin/cli/runtime.js
@@ -1,9 +1,3 @@
-//Provides: unix_isatty
-function unix_isatty(fd) {
-  // TODO: This is available but unreleased.
-  return typeof require == "function" && require("tty").isatty(fd);
-}
-
 //Provides: unix_mkdir
 function unix_mkdir(name, flags) {
   require("fs").mkdirSync(name.toString(), { mode: flags });
