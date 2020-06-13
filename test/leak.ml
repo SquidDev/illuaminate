041b6347232ad3593e696dbd9adef326568b1d53
Jonathan Coates <git@squiddev.cc>
2020-06-13 17:38:11 +0100

    Only do GCs on the first and last pass
    This significantly reduces the time taken to run tests, as we perform
    far fewer GC passes.

--- test/leak.ml
+++ test/leak.ml
@@ -1,3 +1,7 @@
+let pp_size out x =
+  if x < 0 then Format.fprintf out "-%a" Fmt.bi_byte_size (-x)
+  else Format.fprintf out "+%a" Fmt.bi_byte_size x
+
 type ('a, 'b) action =
   | One : string * ('a -> 'b) -> ('a, 'b) action
   | Action : ('a, 'b) action * ('b, 'c) action -> ('a, 'c) action
@@ -6,33 +10,39 @@ let action ~name f = One (name, f)
 
 let ( >-> ) f g = Action (f, g)
 
-let pp_size out x =
-  if x < 0 then Format.fprintf out "-%a" Fmt.bi_byte_size (-x)
-  else Format.fprintf out "+%a" Fmt.bi_byte_size x
+let rec eval : 'a 'b. ('a, 'b) action -> 'a -> 'b =
+ fun f s ->
+  match f with
+  | One (_, f) -> f s
+  | Action (f, g) -> eval f s |> eval g
 
-let run ?(n = 1000) ?(threshold = 100_000) ~init actions =
-  let stats = Hashtbl.create 16 in
+let rec eval_trace : 'a 'b. ('a, 'b) action -> 'a -> 'b =
+ fun f s ->
+  match f with
+  | One (name, f) ->
+      let before = Gc.stat () in
+      let result = f s in
+      Gc.full_major ();
+      let after = Gc.stat () in
 
-  (* Evaluate a action once, and then n times. After each leaf, we update the memory change for that
-     action. *)
-  let rec eval : 'a 'b. ('a, 'b) action -> Gc.stat * 'a -> Gc.stat * 'b =
-   fun f ((before, s) as ss) ->
-    match f with
-    | One (name, f) ->
-        let result = f s in
-        Gc.full_major ();
-        let after = Gc.stat () in
+      Format.printf "| %30s | %a\n" name pp_size (after.live_words - before.live_words);
+      result
+  | Action (f, g) -> eval_trace f s |> eval_trace g
 
-        let previous = Hashtbl.find_opt stats name |> Option.value ~default:0 in
-        previous + (after.live_words - before.live_words) |> Hashtbl.replace stats name;
-        (after, result)
-    | Action (f, g) -> eval f ss |> eval g
-  in
-  let rec eval_n n state = if n <= 0 then state else eval actions state |> eval_n (n - 1) in
+let run ?(n = 1000) ?(threshold = 100_000) ~init:state actions =
+  (* We run the iteration once - hopefully should get us in a stable state. Then we run n times,
+     monitoring memory before and after. If we think we have a leak, then we run a final time,
+     tracking how much data is allocated. *)
+  let state = eval actions state in
 
   Gc.full_major ();
   let before = Gc.stat () in
-  let after, _ = eval_n n (before, init) in
+
+  let rec eval_n n state = if n <= 0 then state else eval actions state |> eval_n (n - 1) in
+  let state = eval_n n state in
+
+  Gc.full_major ();
+  let after = Gc.stat () in
 
   let change = after.live_words - before.live_words in
   let msg =
@@ -40,15 +50,9 @@ let run ?(n = 1000) ?(threshold = 100_000) ~init actions =
       pp_size after.live_words pp_size change
   in
 
-  (* Dump the memory usage of each pass. *)
-  let rec go : 'a 'b. ('a, 'b) action -> unit = function
-    | One (name, _) ->
-        Hashtbl.find_opt stats name |> Option.value ~default:0
-        |> Format.printf "| %30s | %a\n" name pp_size
-    | Action (f, g) -> go f; go g
-  in
-  go actions;
-
-  if change > threshold then Alcotest.fail msg else Printf.printf "%s\n" msg
+  if change > threshold then (
+    eval_trace actions state |> ignore;
+    Alcotest.fail msg )
+  else Printf.printf "%s\n" msg
 
 let run_unit = run ~init:()
