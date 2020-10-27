11c01efac9a68568e39a0a7fc92e1730b933f3d7
Jonathan Coates <git@squiddev.cc>
2020-10-27 17:43:53 +0000

    Fix esy.json file
    Woops, I'd entirely forgotten how this worked. I need to work on this
    project a little more :).

--- src/tools/esy.ml
+++ src/tools/esy.ml
@@ -4,7 +4,7 @@ module J = Yojson.Safe
 module Versions = struct
   let omnomnom = "4a7020d72ee63e241955aec27c8fded59813b792"
 
-  let lsp = "e129cfe315d197deb8f1ce93ea54158e784603f2"
+  let lsp = "2b39bb4fb1ee0b65e159bde0dd871ff1456d0461"
 end
 
 let rec json_of_value : value -> J.t = function
@@ -101,8 +101,9 @@ let () =
                   `String ("git://github.com/SquidDev/omnomnom:omnomnom.opam#" ^ Versions.omnomnom)
                 );
                 ( "@opam/lsp",
-                  `String ("git://github.com/ocaml/ocaml-lsp.git#" ^ Versions.lsp)
-                )
+                  `String ("git://github.com/ocaml/ocaml-lsp.git:lsp.opam#" ^ Versions.lsp) );
+                ( "@opam/jsonrpc",
+                  `String ("git://github.com/ocaml/ocaml-lsp.git:jsonrpc.opam#" ^ Versions.lsp) )
               ] );
           ( "scripts",
             `Assoc
