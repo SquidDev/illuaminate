a139d82cad28f0b8d8346e3ce6b1251f3c7a46e4
Jonathan Coates <git@squiddev.cc>
2022-08-04 17:41:31 +0100

    Publish everything via GH actions
    And with an actual versioning scheme!

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -7,7 +7,11 @@ mkdir -p "$HOME/.ssh/"
 echo "$SSH_KEY" > "$HOME/.ssh/key"
 chmod 600 "$HOME/.ssh/key"
 
+OUT_DIR="$(git describe --always --abbrev=7)"
+mkdir "$OUT_DIR"
+mv artifacts/*/* "$OUT_DIR"
+
 # And upload
 rsync -avc -e "ssh -i $HOME/.ssh/key -o StrictHostKeyChecking=no -p $SSH_PORT" \
-      $GITHUB_WORKSPACE/*.exe \
-      "$SSH_USER@$SSH_HOST:/var/www/squiddev.cc/illuaminate/windows-x86-64/"
+      "$GITHUB_WORKSPACE/$OUT_DIR" \
+      "$SSH_USER@$SSH_HOST:/var/www/squiddev.cc/illuaminate/bin"
