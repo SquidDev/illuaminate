32ff09cf43025fcc6e6e5c0a41b203d4fd1be8eb
Jonathan Coates <git@squiddev.cc>
2023-08-23 00:39:20 +0100

    Remove omd

--- vendor/omd/src/block.mli
+++
@@ -1,9 +0,0 @@
-open Ast
-
-module Pre : sig
-  val of_channel :
-    in_channel -> (attributes, string) Raw.block list * attributes link_def list
-
-  val of_string :
-    string -> (attributes, string) Raw.block list * attributes link_def list
-end
