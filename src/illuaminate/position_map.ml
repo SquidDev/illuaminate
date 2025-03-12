8becb81c1883fb6fb2882bd7b18f7883882ff2db
Jonathan Coates <git@squiddev.cc>
2025-03-12 21:14:20 +0000

    Use a vector for the position map
    This avoids all the additional allocations from an int map. This reduces
    the number of allocated bytes by ~4%.

--- src/illuaminate/position_map.ml
+++ src/illuaminate/position_map.ml
@@ -1,12 +1,10 @@
-module IntMap = Map.Make (Int)
-
 type pos = Pos of int [@@unboxed]
 type line = int
 type column = int
 
 type store =
-  | Array of int array
-  | Map of int IntMap.t * Lexing.lexbuf
+  | Array of int IArray.t
+  | Map of int CCVector.vector * Lexing.lexbuf
 
 type t = { mutable store : store }
 
@@ -15,29 +13,29 @@ module Builder = struct
 
   let new_line = function
     | { store = Array _; _ } -> failwith "new_line: Called after finalising"
-    | { store = Map (lines, buf); _ } as st ->
+    | { store = Map (lines, buf); _ } ->
         Lexing.new_line buf;
         let { Lexing.pos_lnum; pos_bol; _ } = buf.lex_curr_p in
-        st.store <- Map (IntMap.add (pos_lnum - 1) pos_bol lines, buf)
+        if pos_lnum - 1 <> CCVector.length lines then
+          failwith "new_line: non-sequential line numbers";
+        CCVector.push lines pos_bol
 
   let using buf f =
     if not (Lexing.with_positions buf) then
       invalid_arg "using: Lexing.lexbuf should be tracking positions";
     buf.lex_curr_p <- { buf.lex_curr_p with pos_fname = "input" };
-    let lines = { store = Map (IntMap.singleton 0 0, buf) } in
+    let vec = CCVector.create_with ~capacity:32 0 in
+    CCVector.push vec 0;
+    let lines = { store = Map (vec, buf) } in
 
     let res = f lines lines in
 
-    (* Update the backing map to an array. Should be faster to search than a tree. *)
+    (* Update the backing map to an immutable array. *)
     match lines.store with
     | Array _ -> failwith "using: lines has become an array"
     | Map (xs, _) ->
-        let current = buf.lex_curr_p in
-        let xs = IntMap.add current.pos_lnum current.pos_cnum xs in
-
-        let arr = Array.make (fst (IntMap.max_binding xs) + 1) 0 in
-        IntMap.iter (fun line pos -> arr.(line) <- pos) xs;
-        lines.store <- Array arr;
+        new_line lines;
+        lines.store <- Array (CCVector.to_array xs |> IArray.of_array);
         (res, lines)
 end
 
@@ -53,9 +51,8 @@ let find ~offset idx =
 
 let find_line ~offset = function
   | _ when offset <= 0 -> (0, 0)
-  | { store = Array xs; _ } -> find ~offset (Array.get xs) 0 (Array.length xs - 2)
-  | { store = Map (xs, _); _ } ->
-      find ~offset (Fun.flip IntMap.find xs) 0 (IntMap.max_binding xs |> fst)
+  | { store = Array xs; _ } -> find ~offset (IArray.get xs) 0 (IArray.length xs - 2)
+  | { store = Map (xs, _); _ } -> find ~offset (CCVector.get xs) 0 (CCVector.length xs - 2)
 
 let position_of lines (Pos offset) =
   let line, bol = find_line ~offset lines in
@@ -68,7 +65,7 @@ let position_bol lines (Pos offset) =
 let max_position lines =
   let pos =
     match lines with
-    | { store = Array xs; _ } -> xs.(Array.length xs - 1)
+    | { store = Array xs; _ } -> IArray.get xs (IArray.length xs - 1)
     | { store = Map _; _ } -> Int.max_int
   in
   Pos pos
